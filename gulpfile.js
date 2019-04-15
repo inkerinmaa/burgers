e22e835b630c1739812e4ff13f8342b2d5cbb07a
inkerinmaa <techniker357@gmail.com>
2019-04-15 16:04:20 +0300

    gulp automation added

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const gulp = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
 
 const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
 const groupMediaQueries = require('gulp-group-css-media-queries');
 const cleanCSS = require('gulp-cleancss');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
@@ -18,116 +20,131 @@ const del = require('del');
 const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync').create();
 
+const svgstore = require('gulp-svgstore');
+const svgmin = require('gulp-svgmin');
+const imagemin = require('gulp-imagemin');
+
 const paths =  {
     src: './src/',              // paths.src
     build: './build/',          // paths.build
-    modules: './node_modules/'
+    modules: './node_modules'
 };
 
 function styles() {
-    return gulp.src([
-        paths.modules + 'normalize.css/normalize.css',
-        paths.src + 'scss/main.scss'
-    ])
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sassGlob())
-        .pipe(sass()) // { outputStyle: 'compressed' }
-        .pipe(groupMediaQueries())
-        .pipe(cleanCSS())
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(sourcemaps.write('/'))
-        .pipe(gulp.dest(paths.build + 'css/'))
-}
+    return src(paths.src + 'scss/style.scss')
+      .pipe(plumber())
+      .pipe(sourcemaps.init())
+      .pipe(sassGlob())
+      .pipe(sass()) // { outputStyle: 'compressed' }
+      .pipe(groupMediaQueries())
+      .pipe(postcss([
+        autoprefixer({browsers: ['last 2 version']}),
+      ]))
+      .pipe(cleanCSS())
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(sourcemaps.write('/'))
+      .pipe(dest(paths.build + 'css/'));
+  }
 
+function svgSprite() {
+    return src(paths.src + 'svg/*.svg')
+      .pipe(svgmin(function (file) {
+        return {
+          plugins: [{
+            cleanupIDs: {
+              minify: true
+            }
+          }]
+        }
+      }))
+      .pipe(svgstore({ inlineSvg: true }))
+      .pipe(rename('sprite-svg.svg'))
+      .pipe(dest(paths.build + 'img/'));
+}
+  
 function scripts() {
-    return gulp.src([
-        paths.modules + 'jquery/dist/jquery.min.js',
-        paths.modules + 'jquery-touchswipe/jquery.touchSwipe.min.js',
-        paths.modules + 'mobile-detect/mobile-detect.min.js',
-        paths.src + 'js/*.js'
-    ])
-        .pipe(plumber())
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(uglify())
-        .pipe(concat('script.min.js'))
-        .pipe(gulp.dest(paths.build + 'js/'))
+    return src(paths.src + 'js/*.js')
+      .pipe(plumber())
+      .pipe(babel({
+        presets: ['@babel/env']
+      }))
+      .pipe(uglify())
+      .pipe(concat('script.min.js'))
+      .pipe(dest(paths.build + 'js/'))
+  }
+function scriptsVendors() {
+    return src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/slick-carousel/slick/slick.min.js',
+        'node_modules/svg4everybody/dist/svg4everybody.min.js'
+      ])
+      .pipe(concat('vendors.min.js'))
+      .pipe(dest(paths.build + 'js/'))
 }
-
 function htmls() {
-    return gulp.src(paths.src + '*.html')
-        .pipe(plumber())
-        .pipe(replace(/\n\s*<!--DEV[\s\S]+?-->/gm, ''))
-        .pipe(gulp.dest(paths.build));
-}
+    return src(paths.src + '*.html')
+      .pipe(plumber())
+      .pipe(replace(/\n\s*<!--DEV[\s\S]+?-->/gm, ''))
+      .pipe(dest(paths.build));
+  }
 
 function fonts() {
-    return gulp.src(paths.src + 'fonts/**/*')
+    return src(paths.src + 'fonts/**/*')
         .pipe(plumber())
-        .pipe(gulp.dest(paths.build + 'fonts/'));
+        .pipe(dest(paths.build + 'fonts/'));
 }
 function icons() {
-    return gulp.src(paths.src + 'icons/**/*')
+    return src(paths.src + 'icons/**/*')
         .pipe(plumber())
-        .pipe(gulp.dest(paths.build + 'icons/'));
+        .pipe(dest(paths.build + 'icons/'));
 }
-function img() {
-    return gulp.src(paths.src + 'img/**/*')
-        .pipe(plumber())
-        .pipe(gulp.dest(paths.build + 'img/'));
-}
+function images() {
+    return src(paths.src + 'img/*.{jpg,jpeg,png,gif,svg}')
+      .pipe(imagemin())
+      .pipe(dest(paths.build + 'img/'));
+  }
 
 function php() {
-    return gulp.src(paths.src + '*.php')
+    return src(paths.src + '*.php')
         .pipe(plumber())
-        .pipe(gulp.dest(paths.build));
+        .pipe(dest(paths.build));
 }
 
 function clean() {
     return del('build/')
 }
 
-function watch() {
-    gulp.watch(paths.src + 'scss/*.scss', styles);
-    gulp.watch(paths.src + 'js/*.js', scripts);
-    gulp.watch(paths.src + '*.html', htmls);
-}
-
+function watcher() {
+    watch(paths.src + 'scss/*.scss', styles);
+    watch(paths.src + 'js/*.js', scripts);
+    watch(paths.src + '*.html', htmls);
+  }
+  
 function serve() {
     browserSync.init({
-        server: {
-            baseDir: paths.build
-        }
+      server: {
+        baseDir: paths.build
+      }
     });
     browserSync.watch(paths.build + '**/*.*', browserSync.reload);
 }
 
 exports.styles = styles;
 exports.scripts = scripts;
+exports.scriptsVendors = scriptsVendors;
 exports.htmls = htmls;
-exports.fonts = fonts;
-exports.icons = icons;
-exports.img = img;
-exports.php = php;
+exports.images = images;
+exports.svgSprite = svgSprite;
 exports.clean = clean;
-exports.watch = watch;
+exports.watcher = watcher;
 
-gulp.task('build', gulp.series(
-    clean,
-    styles,
-    scripts,
-    htmls,
-    fonts,
-    icons,
-    img,
-    php
-    // gulp.parallel(styles, scripts, htmls)
-));
+exports.build = series(
+  clean,
+  parallel(styles, svgSprite, scripts, scriptsVendors, htmls, images)
+);
 
-gulp.task('default', gulp.series(
-    clean,
-    gulp.parallel(styles, scripts, htmls, fonts, icons, img, php),
-    gulp.parallel(watch, serve)
-));
+exports.default = series(
+  clean,
+  parallel(styles, svgSprite, scripts, scriptsVendors, htmls, images),
+  parallel(watcher, serve),
+);
